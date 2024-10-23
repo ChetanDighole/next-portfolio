9c851ba3130638e875b351712c87da5d306314e0
ChetanDighole <digholechetan@gmail.com>
2024-10-23 17:12:42 +0530

    skills designed

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -11,8 +11,10 @@ export default function HeroSection() {
 
             <div className='mt-6 lg:mt-0'>
                 <p className='text-[#9E9E9E] text-lg md:text-xl lg:text-3xl font-light'>Hi there! I am</p>
-                <h1 className='text-[32px] md:text-[46px] lg:text-[64px] font-semibold' >CHETAN DIGHOLE</h1>
-                <p className='text-xl md:text-2xl lg:text-3xl font-medium'>Full-Stack Web Developer</p>
+                {/* <h1 className='text-[32px] md:text-[46px] lg:text-[64px] font-semibold' >CHETAN DIGHOLE</h1> */}
+                <h1 className='text-4xl md:text-6xl font-light' >CHETAN DIGHOLE</h1>
+                {/* <p className='text-xl md:text-2xl lg:text-3xl font-medium'>Full-Stack Web Developer</p> */}
+                <p className='font-[400] md:font-[500] text-[24px] md:text-[28px]'>Full-Stack Web Developer</p>
                 <p className='text-lg md:text-xl font-normal'> Bringing websites to life, one pixel at a time.</p>
                 <p className='text-lg md:text-xl font-normal flex items-center gap-2'>Mumbai, India <Image alt='heart' src={yellowHeart} className='w-7 h-7' /></p>
             </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,9 +1,33 @@
-export default function Skills() {
+import { PrismaClient } from "@prisma/client";
+
+export default async function Skills() {
+  const prisma = new PrismaClient();
+
+  const data = await prisma.skill.findMany({});
+
   return (
     <div className=" px-4 lg:px-16 pt-7 pb-14">
-      <div className="text-2xl lg:text-4xl font-medium">Magic Happens Here</div>
-      <div className="text-lg md:text-xl font-normal mt-1 lg:mt-2">
+      <div className="text-4xl md:text-6xl font-light">Magic Happens Here</div>
+      {/* <div className="text-lg md:text-xl font-normal mt-1 lg:mt-2">
         A Collection of Tools and Technologies I Use to Bring Ideas to Life
+      </div> */}
+
+      <div className="flex flex-col md:flex-row items-start md:items-center gap-4 font-[400] md:font-[500] text-[16px] md:text-[20px] pb-10 md:pb-[116px] lg:pb-16">
+        A Collection of Tools and Technologies I Use to Bring Ideas to Life
+        <span className="bg-[#9E9E9E] h-[1px] w-full md:w-[273px] opacity-40"></span>
+      </div>
+
+      <div className="grid grid-cols-4 md:grid-cols-6 justify-evenly items-center w-full text-center gap-4">
+        {data.map((eachEle, index) => (
+          <div className="flex flex-col w-max" key={eachEle.id}>
+            <img
+              alt={`${eachEle.title} logo`}
+              src={eachEle.image}
+              className="w-14 h-14 sm:w-20 sm:h-20 filter grayscale hover:grayscale-0 transition-filter duration-300 ease-out"
+            />
+            <h4>{eachEle.title}</h4>
+          </div>
+        ))}
       </div>
     </div>
   );

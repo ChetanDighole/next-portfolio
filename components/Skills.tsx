b3771c60373d77a7158b06890054c5a3136e394a
ChetanDighole <digholechetan@gmail.com>
2024-11-01 16:45:22 +0530

    skills card ui

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -16,11 +16,14 @@ export default async function Skills() {
 
       <div className="grid grid-cols-4 md:grid-cols-6 justify-evenly items-center w-full text-center gap-4">
         {data.map((eachEle, index) => (
-          <div className="flex flex-col w-max" key={eachEle.id}>
+          <div
+            className="filter grayscale hover:grayscale-0 flex flex-col w-max border border-gray-300 rounded-md shadow-md p-4 transition-transform duration-300 hover:scale-105"
+            key={eachEle.id}
+          >
             <img
               alt={`${eachEle.title} logo`}
               src={eachEle.image}
-              className="w-14 h-14 sm:w-20 sm:h-20 filter grayscale hover:grayscale-0 transition-filter duration-300 ease-out"
+              className="w-14 h-14 sm:w-20 sm:h-20 transition-filter duration-300 ease-out"
             />
             <h4>{eachEle.title}</h4>
           </div>

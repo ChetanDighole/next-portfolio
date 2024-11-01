f3c0b8081bbefed50b2f9b7ddef13a8955b6d79b
ChetanDighole <digholechetan@gmail.com>
2024-11-02 00:40:30 +0530

    email done

---
+++ app/api/extra/email/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+export async function POST(req: Request) {
+  try {
+    const { email, subject, message } = await req.json();
+    await sendEmail(email, subject, message);
+    return NextResponse.json({
+      message: "email sent successfully",
+      success: true,
+    });
+  } catch {
+    return NextResponse.json({
+      message: "catch at email post",
+      success: false,
+    });
+  }
+}
+
+async function sendEmail(sender: string, subject: string, message: string) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: sender,
+    to: process.env.EMAIL_USER,
+    subject: subject,
+    html: `customer mail := ${sender}, <br>
+    customer message := ${message}
+`,
+  });
+}
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,45 @@
+"use client"
+import { useState } from "react"
+
 export default function Footer() {
+
+    const [emailData, setEmailData] = useState({
+        email: '',
+        subject: '',
+        message: ''
+    })
+
+    const handleChange = (e) => {
+        setEmailData({
+            ...emailData,
+            [e.target.name]: e.target.value
+        })
+        console.log(emailData)
+    }
+
+    const submitMail = async () => {
+        const res = await fetch('/api/extra/email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: emailData.email,
+                subject: emailData.subject,
+                message: emailData.message
+            })
+        })
+
+        setEmailData({
+            email: '',
+            subject: '',
+            message: ''
+        })
+
+
+    }
+
+
     return (
         <footer className="bg-[#0F0F0F] text-white px-4 lg:px-16 pt-7 pb-14">
             <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 gap-8">
@@ -9,19 +50,19 @@ export default function Footer() {
                 </div>
                 <div>
                     <div className="space-y-4">
-                        <input type="email" placeholder="Your Email"
+                        <input type="email" placeholder="Your Email" onChange={handleChange} name="email" value={emailData.email}
                             className="w-full bg-slate-800 p-2 rounded text-gray-100 border border-gray-600 placeholder-gray-400"
                         />
 
-                        <input type="text" placeholder="Subject"
+                        <input type="text" placeholder="Subject" onChange={handleChange} name="subject" value={emailData.subject}
                             className="w-full bg-slate-800 p-2 rounded text-gray-100 border border-gray-600 placeholder-gray-400"
                         />
 
-                        <textarea placeholder="Message"
+                        <textarea placeholder="Message" onChange={handleChange} name="message" value={emailData.message}
                             className="w-full bg-slate-800 p-2 rounded text-gray-100 border border-gray-600 placeholder-gray-400"
                             rows="3" ></textarea>
 
-                        <button className="bg-[#F5F5F5] text-[#0F0F0F] rounded-md shadow-md hover:shadow-lg active:shadow-inner active:translate-y-1 transition-all font-mono px-4 h-12 text-lg w-full">
+                        <button onClick={() => submitMail()} className="bg-[#F5F5F5] text-[#0F0F0F] rounded-md shadow-md hover:shadow-lg active:shadow-inner active:translate-y-1 transition-all font-mono px-4 h-12 text-lg w-full">
                             Send
                         </button>
 
